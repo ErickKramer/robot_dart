8af200f26a57c21dac65948565b9850474376180
Erick Kramer <erick.romero.kramer@fkie.fraunhofer.de>
2019-08-15 18:04:35 +0200

    Displacement of the robot body in the simulation. Pending rotation.

--- src/arm_sim/dummy_visualizer.cpp
+++ src/arm_sim/dummy_visualizer.cpp
@@ -14,8 +14,15 @@ int main(){
     // Creates simulation
     robot_dart::RobotDARTSimu simu(0.001);
 
-    std::vector<std::pair<std::string, std::string>> packages = {{"fetch", 
-        std::string(RESPATH) + "/models/meshes/fetch"}};
+    // Indicated the path where the meshes files are located 
+    // std::vector<std::pair<std::string, std::string>> packages = {{"fetch", 
+    //     std::string(RESPATH) + "/models/meshes/fetch"}};
+    
+    // std::vector<std::pair<std::string, std::string>> packages = {{"iiwa14", 
+    //     std::string(RESPATH) + "/models/meshes/iiwa14"}};
+
+    std::vector<std::pair<std::string, std::string>> packages = {{"schunk", 
+        std::string(RESPATH) + "/models/meshes/lwa4d"}};
 
     #ifdef GRAPHIC
         // Generates the world
@@ -28,21 +35,41 @@ int main(){
 
 
     // Read the robot from the urdf
-    auto dummy_robot = std::make_shared<robot_dart::Robot>("res/models/fetch.urdf", packages);
+    // auto dummy_robot = std::make_shared<robot_dart::Robot>("res/models/fetch_arm.urdf", packages, "dummy_robot");
+    // auto dummy_robot = std::make_shared<robot_dart::Robot>("res/models/iiwa14.urdf", packages, "dummy_robot");
+    auto dummy_robot = std::make_shared<robot_dart::Robot>("res/models/arm_schunk_with_collisions.urdf", packages, "dummy_robot");
 
-    simu.add_floor(10., 0.2);
+    // simu.add_floor(10., 0.2);
+
+    // Load collision moduled
     simu.world() -> getConstraintSolver() ->
         setCollisionDetector(dart::collision::FCLCollisionDetector::create());
+    
     // Pin robot to the world
     dummy_robot -> fix_to_world();
     dummy_robot -> set_position_enforced(true);
+    
+    // Display the name of the robot
+    std::cout << "Simulating the robot " << dummy_robot -> name() << std::endl;
 
-    // Add robot to the simulator
+    // Display the number of DoF
+    std::cout << "Number of DoF " << dummy_robot -> skeleton() -> getNumDofs() << std::endl;
+
     simu.add_robot(dummy_robot);
+    simu.run(3.);
 
+    // Vector to move the position of the arm in the world
+    Eigen::Isometry3d tf = Eigen::Isometry3d::Identity();
+    tf.translation() = Eigen::Vector3d(0, 0.0, -0.5);
+    // tf.rotation() = Eigen::Matrix3d()
+
+    // dummy_robot -> skeleton() -> setTransformFromParentBodyNode(tf);
+    dummy_robot -> skeleton() -> getRootBodyNode() -> getParentJoint() ->
+        setTransformFromParentBodyNode(tf);
+    // Add robot to the simulator
 
 
     // Runs simulator for 5 seconds
-    simu.run(5.);
+    simu.run(3.);
     return 0;
 }
