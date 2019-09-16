377a345b9dbad6e7a3ebadef57fc3d47ff184c4c
erickkramer <erickkramer@gmail.com>
2019-09-16 14:00:28 +0200

    Created a PID controller

---
+++ src/robot_dart/control/pid_control.cpp
@@ -0,0 +1,94 @@
+#include "pid_control.hpp"
+#include "robot_dart/robot.hpp"
+#include "robot_dart/utils.hpp"
+
+namespace robot_dart {
+    namespace control {
+        PIDControl::PIDControl() : RobotControl() {}
+        PIDControl::PIDControl(const std::vector<double>& ctrl, bool full_control) : RobotControl(ctrl, full_control) {}
+
+        void PIDControl::configure()
+        {
+            if (_ctrl.size() == _control_dof)
+                _active = true;
+
+            if (_Kp.size() == 0)
+                set_pid(10., 0., 0.1);
+        }
+
+        Eigen::VectorXd PIDControl::calculate(double)
+        {
+            ROBOT_DART_ASSERT(_control_dof == _ctrl.size(), "PIDControl: Controller parameters size is not the same as DOFs of the robot", Eigen::VectorXd::Zero(_control_dof));
+            auto robot = _robot.lock();
+            Eigen::VectorXd target_positions = Eigen::VectorXd::Map(_ctrl.data(), _ctrl.size());
+
+            Eigen::VectorXd q = get_positions();
+            Eigen::VectorXd dq = get_velocities();
+
+            // Calculate error
+            Eigen::VectorXd error = target_positions.array() - q.array();
+
+            // Compute proportional term
+            Eigen::VectorXd Pout = _Kp.array() * error.array();
+
+            // Compute Integral term
+            _integral = _integral.array() + error.array() * robot->skeleton()->getTimeStep(); 
+            Eigen::VectorXd Iout = _Ki.array() * _integral.array();
+
+            // Compute Derivative term 
+            Eigen::VectorXd derivative = (error.array() - _pre_error.array()) / robot->skeleton()->getTimeStep();
+            Eigen::VectorXd Dout = _Kd.array() * derivative.array();
+
+            // Update previous error
+            _pre_error = error;
+
+            // Compute torque Commands 
+            Eigen::VectorXd commands = Pout + Iout + Dout;
+
+
+
+            // std::cout << "Target Positions " << std::endl;
+            // std::cout << target_positions.transpose() << std::endl;
+            // std::cout << "Current Positions " << std::endl;
+            // std::cout << q.transpose() << std::endl;
+            // std::cout << "Current Vel " << std::endl;
+            // std::cout << dq.transpose() << std::endl;
+            // std::cout << "Commands " << std::endl;
+            // std::cout << commands.transpose() << std::endl;
+            // std::cout << "----" << std::endl;
+            return commands;
+        }
+
+        void PIDControl::set_pid(double Kp, double Ki, double Kd)
+        {
+            ROBOT_DART_WARNING(_control_dof != 1, "PIDControl: Setting all the gains to Kp = " << Kp << " Ki = " << Ki << " Kd = " << Kd);
+            _Kp = Eigen::VectorXd::Constant(_control_dof, Kp);
+            _Ki = Eigen::VectorXd::Constant(_control_dof, Ki);
+            _Kd = Eigen::VectorXd::Constant(_control_dof, Kd);
+            _pre_error = Eigen::VectorXd::Zero(_control_dof);
+            _integral = Eigen::VectorXd::Zero(_control_dof);
+        }
+
+        void PIDControl::set_pid(const Eigen::VectorXd& Kp, const Eigen::VectorXd& Ki, const Eigen::VectorXd& Kd)
+        {
+            ROBOT_DART_ASSERT(static_cast<size_t>(Kp.size()) == _control_dof, "PIDControl: The Kp size is not the same as the DOFs!", );
+            ROBOT_DART_ASSERT(static_cast<size_t>(Ki.size()) == _control_dof, "PIDControl: The Kp size is not the same as the DOFs!", );
+            ROBOT_DART_ASSERT(static_cast<size_t>(Kd.size()) == _control_dof, "PIDControl: The Kd size is not the same as the DOFs!", );
+            _Kp = Kp;
+            _Ki = Ki;
+            _Kd = Kd;
+            _pre_error = Eigen::VectorXd::Zero(_control_dof);
+            _integral = Eigen::VectorXd::Zero(_control_dof);
+        }
+
+        std::tuple<Eigen::VectorXd, Eigen::VectorXd, Eigen::VectorXd> PIDControl::pid() const
+        {
+            return std::make_tuple(_Kp, _Ki, _Kd);
+        }
+
+        std::shared_ptr<RobotControl> PIDControl::clone() const
+        {
+            return std::make_shared<PIDControl>(*this);
+        }
+    } // namespace control
+} // namespace robot_dart
---
+++ src/robot_dart/control/pid_control.hpp
@@ -0,0 +1,38 @@
+
+#ifndef ROBOT_DART_CONTROL_PID_CONTROL
+#define ROBOT_DART_CONTROL_PID_CONTROL
+
+#include <Eigen/Core>
+#include <utility>
+
+#include <robot_dart/control/robot_control.hpp>
+
+namespace robot_dart {
+    namespace control {
+
+        class PIDControl : public RobotControl {
+        public:
+            PIDControl();
+            PIDControl(const std::vector<double>& ctrl, bool full_control = false);
+
+            void configure() override;
+            Eigen::VectorXd calculate(double) override;
+
+            void set_pid(double p, double i, double d);
+            void set_pid(const Eigen::VectorXd& p, const Eigen::VectorXd& i, const Eigen::VectorXd& d);
+
+            std::tuple<Eigen::VectorXd, Eigen::VectorXd, Eigen::VectorXd> pid() const;
+
+            std::shared_ptr<RobotControl> clone() const override;
+
+        protected:
+            Eigen::VectorXd _Kp;
+            Eigen::VectorXd _Ki;
+            Eigen::VectorXd _Kd;
+            Eigen::VectorXd _pre_error;
+            Eigen::VectorXd _integral;
+        };
+    } // namespace control
+} // namespace robot_dart
+
+#endif
