1fb5687f40c8cc464bb63fdc34ea3336a0fc0cc5
erickkramer <erickkramer@gmail.com>
2019-09-20 13:08:07 +0200

    Restricted the setcommands to just the joints belonging to the arm, ignoring the gripper

--- src/robot_dart/robot.cpp
+++ src/robot_dart/robot.cpp
@@ -86,7 +86,13 @@ namespace robot_dart {
                 commands += ctrl->weight() * ctrl->commands(t);
         }
 
-        _skeleton->setCommands(commands);
+        // _skeleton->setCommands(commands);
+        // Testing passing indices of the joints to set the commands to 
+        std::vector<std::size_t> indices_arm{0,1,2,3,4,5,6};
+        _skeleton->setCommands(indices_arm, commands.head(7));
+        
+        // _skeleton->setCommand(7, -1);
+        // _skeleton->setCommand(8, -1);
     }
 
     void Robot::reinit_controllers()
